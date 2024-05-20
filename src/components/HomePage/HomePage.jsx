4949d61a3206b87a9e587cdd8c943226714aca63
Anchi <anchi.n248@gmail.com>
2024-05-19 21:49:33 -0500

    ...

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import './HomePage.scss'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <div className='homepage_container'>
+        <div className='homepage-img_wrapper'>
+          <img src="src/assets/Screen Shot 2024-04-24 at 11.43.27 AM.png" alt="" />
+        </div>
+        
+        <div className='short-intro'>
+          short intro
+        </div>
+      </div>
+      
+    </>
   )
 }
 
--- src/components/Navigator/Navigator.jsx
+++ src/components/Navigator/Navigator.jsx
@@ -1,13 +1,20 @@
-import './Navigator.css'
+import './Navigator.scss'
 const Navigator = () => {
   return (
-    <header>
-        <div>Anchi Nguyen</div>
-        <nav>
-            <div>Illustrations</div>
-            <div>Designs</div>
-            <div>About</div>
+    <header className='header_container'>
+        <nav className='navlink_wrapper'>
+            <div>home</div>
+            <div>about</div>
+            <div>projects</div>
+            <div>contact</div>
         </nav>
+
+        <div className='logo'>ANCHI NGUYEN</div>
+
+        <div className='social-link_wrapper'>
+            <i className="fa-brands fa-github"></i>
+            <i className="fa-brands fa-linkedin"></i>
+        </div>
     </header>
   )
 }
